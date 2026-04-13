1949b5e9e40a5a0c8db7d10e479f87d9ee807f84
ideazicsolutions <ideazicsolutions@gmail.com>
2026-04-13 16:01:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ app.post("/booking", async (req, res) => {
   console.log("New Booking:", data);
 
   try {
-    await fetch(https://services.leadconnectorhq.com/hooks/DQwQEDZeR14RV6YlCH1K/webhook-trigger/eda05b3b-029c-41c0-a1c4-608dbf66cf7b", {
-      method: "POST",
+await fetch("https://services.leadconnectorhq.com/hooks/DQwQEDZeR14RV6YlCH1K/webhook-trigger/eda05b3b-029c-41c0-a1c4-608dbf66cf7b", {      method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
